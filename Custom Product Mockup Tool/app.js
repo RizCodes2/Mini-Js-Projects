c4fc96842c3210ac486038dd9478c04a60ba4a52
RizCodes2 <imrizwan164@gmail.com>
2026-04-09 11:35:14 +0500

    code added

---
+++ Custom Product Mockup Tool/app.js
@@ -0,0 +1,9 @@
+const applybtn = document.getElementById("apply-btn");
+
+const black = document.getElementById("black-color");
+const blue = document.getElementById("blue-color");
+const brown = document.getElementById("brown-color");
+const green = document.getElementById("green-color");
+const orange = document.getElementById("orange-color");
+const red = document.getElementById("red-color");
+const white = document.getElementById("white-color");
