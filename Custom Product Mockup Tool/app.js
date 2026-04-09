0b884c9fd4ed774e1ed978da3dda67b1b9b547fd
RizCodes2 <imrizwan164@gmail.com>
2026-04-09 18:14:53 +0500

    correction

--- Custom Product Mockup Tool/app.js
+++ Custom Product Mockup Tool/app.js
@@ -7,3 +7,40 @@ const green = document.getElementById("green-color");
 const orange = document.getElementById("orange-color");
 const red = document.getElementById("red-color");
 const white = document.getElementById("white-color");
+
+const Tshirt = document.getElementById("Tshirt");
+
+
+const new_shirt_text = document.getElementById("Tshirt-text");
+
+
+blue.addEventListener("click", () => {
+    Tshirt.src = "Shirts/blue Shirt.png"
+
+})
+white.addEventListener("click", () => {
+    Tshirt.src = "Shirts/white Shirt.png"
+})
+red.addEventListener("click", () => {
+    Tshirt.src = "Shirts/red Shirt.png"
+})
+orange.addEventListener("click", () => {
+    Tshirt.src = "Shirts/orange Shirt.png"
+})
+green.addEventListener("click", () => {
+    Tshirt.src = "Shirts/green Shirt.png"
+})
+brown.addEventListener("click", () => {
+    Tshirt.src = "Shirts/brown Shirt.png"
+})
+black.addEventListener("click", () => {
+    Tshirt.src = "Shirts/black Shirt.png"
+})
+
+
+applybtn.addEventListener("click", () => {
+    const User_Tshirt_Text = document.getElementById("shirt-user-text").value;
+
+    console.log(User_Tshirt_Text);
+    new_shirt_text.innerText = User_Tshirt_Text;
+})
